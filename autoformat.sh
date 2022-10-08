6b5a4ee7187d35136a223aa8c01af2f67127ccc7
corenel <xxdsox@gmail.com>
2022-10-08 16:00:20 +0800

    build: use CMake build system

--- autoformat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Formatting..."
-root=`dirname ${BASH_SOURCE[0]}`
-echo $root
-time clang-format -style="{BasedOnStyle: llvm, IndentWidth: 4}" -i $root/*.c $root/*.h $root/*.cpp
-echo "Done"
